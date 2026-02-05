b9fcb354e868f9743a398e288b7964c8c79db130
Nerevakiin <djmatisk@yahoo.gr>
2026-02-05 02:15:02 +0200

    suka javaskript

---
+++ public/signup.js
@@ -0,0 +1,19 @@
+const signupForm = document.getElementById('signup-form')
+const errorMessage = document.getElementById('error-message')
+
+signupForm.addEventListener('submit', async (e) => {
+    
+    e.preventDefault() // prevent form from reloading
+
+    const name = document.getElementById('signup-fullname')
+    const email = document.getElementById('signup-email')
+    const username = document.getElementById('signup-username')
+    const password = document.getElementById('signup-password')
+    const submitBtn = document.getElementById('signup-btn')
+
+    
+
+
+
+
+})
