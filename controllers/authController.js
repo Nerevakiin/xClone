8e37bc44fd48887843a0bd0b107baaad5ee0c10a
Nerevakiin <djmatisk@yahoo.gr>
2026-02-05 15:52:39 +0200

    body editing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,33 @@
-// import { validator } from 'validator'
+import { validator } from 'validator'
 // import { getDBConnection } from '../db/db.js'
 // import bcrypt from 'bcryptjs'
 
 export async function registerUser(req, res) {
-    console.log('auth controller reached')
-    console.log('req.body is: ', req.body)
+    
+    console.log('req.body before server side correction: ', req.body)
+    
+    let {name, email, username, password} = req.body // destructuring the body properties
 
-    res.json({message: 'user registered succesfully'})
+    // check if all necessary properties exist, otherwise return
+    if (!!name || !email || !username || !password) {
+        return res.status(400).json({ error: 'All fields are required'})
+    }
+
+    // trim them of unecessary spaces
+    name = name.trim()
+    email = email.trim()
+    username = username.trim()
+
+    // check the username regex requirements
+    if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+        return res.status(400).json({error: 'invalid username'})
+    }
+
+    // check if the email is actually an email
+    if (!validator.isEmail(email)) {
+        res.status(400).json({error: 'invalid email'})
+    }
+
+    console.log('req.body after server side correction: ', req.body)
+
 }
