4a99b319f0a663df94dabb4712f78664377eeb5d
Nerevakiin <djmatisk@yahoo.gr>
2026-02-06 15:44:40 +0200

    added a welcoming front-page

--- public/logout.js
+++ public/logout.js
@@ -1,7 +1,7 @@
 export async function logout() {
     try {
         const res = await fetch('/api/auth/logout')
-        window.location.href = '/'
+        window.location.href = '/frontPage.html'
     } catch (err){
         console.log('failed to log out front end, ', err)
     }
